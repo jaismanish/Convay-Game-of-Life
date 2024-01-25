066d29b8883142293b036531b2018d56d443a1be
Manish Jaiswal <manish.jaiswal_ftc@external.swiggy.in>
2024-01-25 16:36:39 +0530

    test for live cell survive with 2 or 3 neighbors

--- GameOfLifeTest.java
+++ GameOfLifeTest.java
@@ -30,4 +30,22 @@ public class GameOfLifeTest {
         int[][] nextGeneration = game.getGrid();
         assertEquals(0, nextGeneration[1][1]);
     }
+
+    @Test
+    public void testLiveCellSurvivesWithTwoOrThreeNeighbors() {
+        // Given
+        GameOfLife game = new GameOfLife(3, 3);
+        int[][] grid = {
+                {1, 1, 0},
+                {0, 1, 0},
+                {0, 0, 0}
+        };
+        game.initialize(grid);
+
+        game.proceed();
+
+        int[][] nextGeneration = game.getGrid();
+        assertEquals(1, nextGeneration[1][1]);
+    }
+
 }
