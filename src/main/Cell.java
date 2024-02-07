cba04c42bb5be2b895172a079572eabbf8fdf711
Manish Jaiswal <manish.jaiswal_ftc@external.swiggy.in>
2024-02-07 15:23:52 +0530

    Test for Cell creation and status and made it pass

--- src/main/Cell.java
+++ src/main/Cell.java
@@ -3,15 +3,19 @@ package src.main;
 public class Cell {
     private boolean isAlive;
 
-    public Cell(boolean isAlive) {
-        this.isAlive = isAlive;
-    }
-
-    public boolean isAlive() {
-        return isAlive;
+    public Cell() {
+        this.isAlive = false;
     }
 
     public void setAlive(boolean alive) {
-        isAlive = alive;
+        this.isAlive = alive;
+    }
+    public boolean isAlive() {
+        return this.isAlive;
+    }
+
+    @Override
+    public String toString() {
+        return this.isAlive ? "1" : "0";
     }
 }
--- src/test/CellTest.java
+++ src/test/CellTest.java
@@ -7,22 +7,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CellTest {
     @Test
-    void cellCanBeInitializedAsAlive() {
-        Cell cell = new Cell(true);
+    void testCellCreated() {
+        Cell cell = new Cell();
+        assertNotNull(cell);
+    }
+
+    @Test
+    void testIsCellAlive_ShouldReturnTrue() {
+        Cell cell = new Cell();
+        cell.setAlive(true);
         assertTrue(cell.isAlive());
     }
 
     @Test
-    void cellCanBeInitializedAsDead() {
-        Cell cell = new Cell(false);
+    void testIsCellDead_ShouldReturnFalse() {
+        Cell cell = new Cell();
         assertFalse(cell.isAlive());
     }
 
     @Test
-    void cellStateCanBeChanged() {
-        Cell cell = new Cell(true);
-        cell.setAlive(false);
-        assertFalse(cell.isAlive());
+    void testMarkingDeadCell_0_AndLiveCell_1() {
+        Cell cell = new Cell();
+        assertEquals("0", cell.toString());
+        cell.setAlive(true);
+        assertEquals("1", cell.toString());
     }
-
 }
