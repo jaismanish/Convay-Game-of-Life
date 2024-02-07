db01c14badff43fc8d10d50d8408a6ed68f07a4d
Manish Jaiswal <manish.jaiswal_ftc@external.swiggy.in>
2024-02-07 21:28:59 +0530

    test and implementation for seeding board

--- src/main/Board.java
+++ src/main/Board.java
@@ -1,5 +1,7 @@
 package src.main;
 
+import java.util.Random;
+
 public class Board {
     private Cell[][] cells;
     private final int rows;
@@ -10,6 +12,42 @@ public class Board {
         this.rows = rows;
         this.cols = cols;
         this.cells = new Cell[rows][cols];
+        initializeCells();
+    }
+
+    private void initializeCells() {
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                this.cells[row][col] = new Cell();
+            }
+        }
+    }
+
+    public void seedPopulation(double seedPercentage) {
+        if (seedPercentage <= 0.0) {
+            throw new IllegalArgumentException("Please provide valid seed percent");
+        }
+        Random random = new Random();
+
+        for (Cell[] row : cells) {
+            for (Cell cell : row) {
+                double seed = random.nextDouble();
+                cell.setAlive(seed <= seedPercentage);
+            }
+        }
+    }
+
+    public void print() {
+        for (Cell[] row : cells) {
+            for (Cell cell : row) {
+                System.out.print(cell.isAlive() ? "O " : ". ");
+            }
+            System.out.println();
+        }
+    }
+
+    public Cell[][] cells() {
+        return this.cells;
     }
 
 }
--- src/test/BoardTest.java
+++ src/test/BoardTest.java
@@ -3,11 +3,9 @@ package src.test;
 import org.junit.jupiter.api.Test;
 import src.main.Board;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BoardTest {
-
 
     @Test
     void testBoardCreatedWithValidSize() {
@@ -22,5 +20,34 @@ public class BoardTest {
         assertThrows(IllegalArgumentException.class, () -> new Board(8, 0));
         assertThrows(IllegalArgumentException.class, () -> new Board(-8, 8));
         assertThrows(IllegalArgumentException.class, () -> new Board(8, -8));
+    }
+    @Test
+    void testSeedingWithZeroSeedPercent_ShouldThrowException() {
+        Board board = new Board(8, 8);
+        double seedPercentage = 0.0;
+        assertThrows(IllegalArgumentException.class, () -> board.seedPopulation(seedPercentage));
+    }
+    @Test
+    void testSeedingWithNegativeSeedPercent_ShouldThrowException() {
+        Board board = new Board(8, 8);
+        double seedPercentage = -0.3;
+        assertThrows(IllegalArgumentException.class, () -> board.seedPopulation(seedPercentage));
+    }
+    @Test
+    void testSeedPopulation() {
+        Board board = new Board(8, 8);
+        int alive = 0;
+        board.seedPopulation(0.3);
+
+        for (int i = 0; i < board.cells().length; i++) {
+            for (int j = 0; j < board.cells()[0].length; j++) {
+                if(board.cells()[i][j].isAlive()) {
+                    ++alive;
+                }
+            }
+        }
+        double actual = (double)alive / 64;
+
+        assertTrue(0.1 > actual - 0.3);
     }
 }
